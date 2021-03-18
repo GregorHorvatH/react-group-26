af7a85f5dd7199d7345b17f73f9b48dac21fff81
Geri <gregor.horvath83@gmail.com>
2021-03-18 20:17:51 +0200

    Updates

---
+++ static/js/main.10cce182.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-group-26"]=this["webpackJsonpreact-group-26"]||[]).push([[0],{9:function(e,r,t){"use strict";t.r(r);t(1);var c=t(3),n=t.n(c),s=t(0);var o=function(){return Object(s.jsx)("div",{className:"App",children:Object(s.jsx)("h1",{children:"Hello from react"})})};n.a.render(Object(s.jsx)(o,{}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.10cce182.chunk.js.map
